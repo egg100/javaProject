696168a0ae5a47ecd2feb113a67ad99d7c471233
egg100 <68427587+egg100@users.noreply.github.com>
2021-02-23 00:20:09 +0900

    exercise operators

---
+++ javaProject/src/operators/Exercise06.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class Exercise06 {
+
+	public static void main(String[] args) {
+		int lengthTop = 5;
+		int lengthBottom = 10;
+		int height = 7;
+		double area = ((lengthTop + lengthBottom) * height * 5) / 10.0;
+		System.out.println(area);
+	}
+
+}
